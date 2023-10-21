7e1c08b45289c6c6975c089bcfd56af88baaf766
raze1_1 <136592924+raze1-1@users.noreply.github.com>
2023-10-22 00:13:13 +0100

    adding boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Edit from './Edit';
-import { Input } from '@material-tailwind/react';
+import { Checkbox, Input } from '@material-tailwind/react';
+import { IonIcon } from '@ionic/react';
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -19,9 +20,9 @@ function App() {
         return { ...task, editing: true };
       }
       return task;
-    })
+    });
 
-    setTasks(updatedTasks)
+    setTasks(updatedTasks);
   };
 
   const addTask = () => {
@@ -31,20 +32,20 @@ function App() {
         editing: false,
       };
 
-    fetch('http://localhost:5000/api/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ description: newTask }),
-    })
-      .then(() => {
-        return fetch('http://localhost:5000/api/tasks'); 
+      fetch('http://localhost:5000/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ description: newTask }),
       })
-      .then((response) => response.json())
-      .then((data) => setTasks(data));
-      
-    setNewTask('');
+        .then(() => {
+          return fetch('http://localhost:5000/api/tasks');
+        })
+        .then((response) => response.json())
+        .then((data) => setTasks(data));
+
+      setNewTask('');
     }
   };
 
@@ -54,15 +55,15 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ description: updatedDescription })
+      body: JSON.stringify({ description: updatedDescription }),
     })
-    .then(() => {
-      return fetch('http://localhost:5000/api/tasks');
-    })
-    .then((response => response.json()))
-    .then((data) => setTasks(data));
+      .then(() => {
+        return fetch('http://localhost:5000/api/tasks');
+      })
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
   };
-  
+
   const deleteTask = (taskId) => {
     fetch(`http://localhost:5000/api/tasks/${taskId}`, {
       method: 'DELETE',
@@ -72,68 +73,108 @@ function App() {
       })
       .then((response) => response.json())
       .then((data) => setTasks(data));
-  }
+  };
 
   const saveTask = (taskId) => {
     const taskToEdit = tasks.find((task) => task._id === taskId);
     const updatedTasks = tasks.map((task) => {
       if (task._id === taskId) {
-        return { ...task, editing:false }
+        return { ...task, editing: false };
       }
       return task;
-    })
+    });
     setTasks(updatedTasks);
     updateTask(taskToEdit._id, taskToEdit.description);
-  }
+  };
 
   const setUpdatedTaskDescription = (taskId, newDescription) => {
     const updatedTasks = tasks.map((task) => {
       if (task._id === taskId) {
-        return { ...task, description: newDescription};
+        return { ...task, description: newDescription };
       }
       return task;
-    })
+    });
     setTasks(updatedTasks);
-  }
+  };
+
+  const completeTask = (taskId) => {
+    fetch(`http://localhost:5000/api/tasks/${taskId}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        return fetch('http://localhost:5000/api/tasks');
+      })
+      .then((response) => response.json())
+      .then((data) => setTasks(data));
+  };
+
 
   return (
     <div className='min-h-screen flex flex-col justify-center items-center'>
       <h1 className='text-blue-500 text-2xl py-3'>Task Manager</h1>
-      <div className=''>
-      <input 
-        type="text"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        placeholder="What's new?"
-        className='mb-3 px-1 py-1'
-      />
+      <div className='mb-2 flex flex-col items-center'>
+        <Input
+          variant='standard'
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          label="What's new?"
+          className='mb-2 px-1 py-1 text-white'
+        />
       </div>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={addTask}>Add Task</button>
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        onClick={addTask}
+      >
+        Add Task
+      </button>
       <ul>
         {tasks.map((task) => (
           <li className='text-white' key={task._id}>
-            {task.editing ? (
-              <div className=''>
-                <Input 
-                  className='text-black'
-                  label='Edit your task...'
-                  value={task.description}
-                  onChange={(e) => setUpdatedTaskDescription(task._id, e.target.value)}
-                />
-                <button className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => saveTask(task._id)}>Save</button>
-              </div>
-            ) : (
-              <div className='space-x-5 mt-7'>
-                {task.description}
-                <button className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => editTask(task._id)}>Edit</button>
-              </div>
-            )}
-            <button className="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => deleteTask(task._id)}>Delete</button>
+            <div className='space-x-5 mt-7 flex flex-row'>
+              <Checkbox
+                checked={completeTask}
+                onChange={() => completeTask(task._id)}
+              />
+              {task.editing ? (
+                <div className=''>
+                  <Input
+                    className='text-white'
+                    variant='static'
+                    color='blue'
+                    label='Edit your task...'
+                    value={task.description}
+                    onChange={(e) => setUpdatedTaskDescription(task._id, e.target.value)}
+                  />
+                  <button
+                    className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 flex flex-row"
+                    onClick={() => saveTask(task._id)}
+                  >
+                    Save
+                  </button>
+                </div>
+              ) : (
+                <div className='flex flex-row justify-center items-center'>
+                  {task.description}
+                  <button
+                    className="flex text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mx-4 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    onClick={() => editTask(task._id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="flex text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    onClick={() => deleteTask(task._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
+            </div>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
