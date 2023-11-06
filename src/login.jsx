b95af761ba0870b8fe853c5406fb7b57cc1d576d
raze1_1 <136592924+raze1-1@users.noreply.github.com>
2023-11-06 15:47:51 +0000

    w

--- src/login.jsx
+++ src/login.jsx
@@ -1,52 +1,16 @@
-import { useState } from "react";
-import { loginFields } from "./constants";
-import FormAction from "./FormAction"
-import FormExtra from "./FormExtra"
-import Input from "./Input";
+import Login from "./Login";
+import Header from "./Header";
 
-const fields = loginFields;
-let fieldsState = {};
-fields.forEach(field=>fieldsState[field.id]="")
-
-export default function Login() {
-  const [loginState,setLoginState] = useState(fieldsState)
-  
-  const handleChange=(e)=>{
-    setLoginState({...loginState, [e.target.id]:e.target.value})
-  }
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    authenticateUser();
-  }
-  
-  const authenticateUser = () =>{
-
-  }
-
+export default function LogIn() {
   return(
-    <form className="mt-8 space-y-6">
-      <div className="-space-y-px">
-        {
-            fields.map(field=>
-              <Input
-                key={field.id}
-                handleChange={handleChange}
-                value={loginState[field.id]}
-                labelText={field.labelText}
-                labelFor={field.labelFor}
-                id={field.id}
-                name={field.name}
-                type={field.type}
-                isRequired={field.isRequired}
-                placeholder={field.placeholder}
-              />
-            )
-        }
-      </div>
-
-      <FormExtra/>
-      <FormAction handleSubmit={handleSubmit} text="Login"/>
-    </form>
+    <>
+      <Header 
+       heading="Login to your account"
+       paragraph="Don't have an account yet?"
+       linkName="Signup"
+       linkUrl="/signup"
+      />
+    <Login/>
+    </>
   )
 }
